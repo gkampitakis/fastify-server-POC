c94fb4260b42dbdfb9fc07b101df84a04e995323
gkampitakis <gkabitakis@gmail.com>
2020-08-02 17:59:13 +0100

    add example health check

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import { Server } from './server';
 import config from './config'
 
 new Server().start(config.server.port)
-  .then((v) => console.log(`Server listening on ${v}`))
-  .catch(err => console.error(err));
+  .addHealthChecks();
--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import cors from 'fastify-cors';
 import auto_load from 'fastify-autoload';
 import path from 'path';
 import config from './config';
+import customHealthCheck from 'fastify-custom-healthcheck';
+import PromiseUtil from '@gkampitakis/promise-util';
 
 export class Server {
 
-  private server;
+  private server: FastifyInstance;
 
   public constructor() {
 
@@ -25,13 +27,39 @@ export class Server {
       .register(auto_load, {
         dir: path.join(__dirname, 'routes'),
         options: {}
+      })
+      .register(customHealthCheck, {
+        path: '/api/health',
+        exposeFailure: true,
+        info: {
+          server: 'Fastify server',
+          usage: 'Experimenting'
+        }
       });
+
+  }
+
+  public async addHealthChecks() {
+
+    await this.server.ready();
+
+    this.server.addHealthCheck('test', () => PromiseUtil.delay(2000, () => {
+      return Promise.reject('Error test')
+    }));
+
+    this.server.addHealthCheck('mongo', () => PromiseUtil.delay(2000, () => Promise.reject('Error')));
+
+    this.server.addHealthCheck('sync', () => true, { value: false });
 
   }
 
   public start(port: number) {
 
-    return this.server.listen(port, '0.0.0.0');
+    this.server.listen(port, '0.0.0.0')
+      .then((v) => console.log(`🚀 Server listening on ${v}`))
+      .catch(err => console.error(err));
+
+    return this;
 
   }
 
